e061d72038e36837df9ef7512a91a55ca5b10dcc
Harmeet <hs978698@gmail.com>
2024-06-05 18:28:47 +0530

    no purpose it was a mistake

--- esehi.py
+++ esehi.py
@@ -35,47 +35,47 @@
 
 
 #5
-#n=float(input()
-#x=n
-#c=0
-#while(x>0):
-#    x=x%10
-#    c+=1
-#    x/=10
-#while(n>0): 
-#    last=n%10
-#    first=n/(10**c)
-#    n/=10
-#print(first)
-#print(last)
+# n=float(input())
+# x=n
+# c=0
+# while(x>0):
+# 	x=x%10
+# 	c+=1
+# 	x/=10
+# while(n>0): 
+# 	last=n%10
+# 	first=n/(10**c)
+# 	n/=10
+# print(first)
+# print(last)
 
 
-#6
+# #6
 
-'''n=eval(input())
-s=str(n)
-f=s[0]+""
-l=s[-1]+""
-f=int(f)
-l=int(l)
-print(f)
-print(l)
-h=f**l
-if(h%2==0):
-    print("True")
-else:
-    print("False")
+# n=eval(input())
+# s=str(n)
+# f=s[0]+""
+# l=s[-1]+""
+# f=int(f)
+# l=int(l)
+# print(f)
+# print(l)
+# h=f**l
+# if(h%2==0):
+#     print("True")
+# else:
+#     print("False")
 
 
 
-#7
+# #7
 
-t1=()
-t1
-l=[]
-l
-t2=(1,2,3,4,5)
-t2'''
+# t1=()
+# t1
+# l=[]
+# l
+# t2=(1,2,3,4,5)
+# t2
 
 
 
@@ -83,13 +83,10 @@ t2'''
 
 a=('A','B')
 b=(1,2)
-a+b
-('A', 'B', 1, 2)
-type(a+b)
-<class 'tuple'>
+print(a+b)
+print(type(a+b))
 t=(1,2,3)
-t*2
-(1, 2, 3, 1, 2, 3)
+print(t*2)
 
 
 t=(76,45,23,11)
@@ -97,30 +94,6 @@ t=list(t)
 t.sort()
 tuple(t)
 (11, 23, 45, 76)
-
-
-t1=('Z','Y','x')
-t2=(26,25,24)
-zip(t1,t2)
-<zip object at 0x000001BA56D68C80>
-
-
-D={}
-type(D)
-<class 'dict'>
-D={'virat kohli':52,'sachin':100}
-D
-{'virat kohli': 52, 'sachin': 100}
-type(D)
-<class 'dict'>
-D['dhoni']=75
-D
-{'virat kohli': 52, 'sachin': 100, 'dhoni': 75}
-del D['dhoni']
-D
-{'virat kohli': 52, 'sachin': 100}
-
-
 
 
     
