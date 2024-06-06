a3d1d4ade73ac9aaa9486a2071e44c3a450efcf4
Harmeet <hs978698@gmail.com>
2024-06-06 13:03:21 +0530

    variables and their types

--- variables.py
+++ variables.py
@@ -7,3 +7,15 @@ myvar2 = "John"
 # 2myvar = "John"
 # my-var = "John"
 # my var = "John"
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
