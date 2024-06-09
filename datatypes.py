246c63aa2a3b7e335c37468cfe7c1d02d42adab6
Harmeet <hs978698@gmail.com>
2024-06-10 00:55:23 +0530

    strings

--- datatypes.py
+++ datatypes.py
@@ -10,3 +10,19 @@ p = "Hello"
 q = 1
 r= str(q)
 print(p+r)
+
+
+a = "Hello, World!"
+b = list(a)
+print(b)
+
+# age = 36
+# txt = "My name is John, I am " + age
+# print(txt)
+
+age = 36
+txt = f"My name is John, I am {age:.2f}"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
