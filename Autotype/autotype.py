ba9bbf6cf1c914de51154b81464fc2e844e80358
Harmeet <hs978698@gmail.com>
2024-06-29 12:23:51 +0530

    updated only path of answers

--- Autotype/autotype.py
+++ Autotype/autotype.py
@@ -2,7 +2,7 @@ import pyautogui, time
 
 time.sleep(5)
 
-f = open("C:/Users/hs978/CodeSpace/Python Programs/answer.txt", 'r')
+f = open("C:/Users/hs978/CodeSpace/Python Programs/Autotype/answer.txt", 'r')
 
 for line in f:
     pyautogui.typewrite(line.strip())
